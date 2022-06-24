35c0a4873d5ac6a090350b6d1337a2e3680b6b15
apd5392 <adoumbia.pas@gmail.com>
2022-06-24 16:41:07 -0400

    Solved Level 1 and 2

--- starter/level-one.js
+++ starter/level-one.js
@@ -2,3 +2,18 @@
  * DOTS: Level One
  *
  */
+let ball = document.querySelector(".js-ball");
+let scoreDisplay = document.querySelector(".js-score")
+let levelWinner = document.querySelector(".level-winner")
+
+let score = 0;
+
+const increaseScore = () => {
+    score += 10
+    scoreDisplay.innerText = score
+    if (score >= 100) {
+        levelWinner.style.opacity = 1
+    }
+}
+
+ball.addEventListener('click', increaseScore)
--- starter/level-two.js
+++ starter/level-two.js
@@ -2,3 +2,24 @@
  * DOTS: Level Two
  *
  */
+
+let ball = document.querySelector(".js-ball");
+let scoreDisplay = document.querySelector(".js-score")
+let levelWinner = document.querySelector(".level-winner")
+
+let score = 0;
+
+
+const elements = document.querySelectorAll('.js-ball');
+for (let i = 0; i < elements.length; i++) {
+  elements[i].addEventListener('click', function() {
+        score += 10
+        scoreDisplay.innerText = score
+        if (score >= 100) {
+            levelWinner.style.opacity = 1
+    }  });
+};
+
+ball.addEventListener('click', increaseScore)
+
+
